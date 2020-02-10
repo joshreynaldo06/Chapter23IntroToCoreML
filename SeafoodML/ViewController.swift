ae497e1bb44bd7679387806b3fad82d289e28461
Josh Reynaldo <joshuaronaldo088@gmail.com>
2020-02-10 15:48:22 +0700

    Initial Commit

---
+++ SeafoodML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeafoodML
+//
+//  Created by Midas on 10/02/20.
+//  Copyright © 2020 Midas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
